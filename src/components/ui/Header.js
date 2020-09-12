961e69ebfefed51de580110d0cebf47a3db033c3
Aizhar John Quindoza <aizhar.quindoza@gmail.com>
2020-09-12 06:10:31 +0400

    Set up header component

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,138 @@
-import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Tabs,
+  Tab,
+  Button,
+} from '@material-ui/core';
 import ElevationScroll from './ElevationScroll';
+import { makeStyles } from '@material-ui/styles';
+
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em',
+  },
+  logo: {
+    height: '8em',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>Arc Development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar disableGutters>
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img className={classes.logo} src={logo} alt="company_logo" />
+            </Button>
+
+            <Tabs
+              value={value}
+              className={classes.tabContainer}
+              onChange={handleChange}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 };
 
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,15 +2,34 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const arcBlue = '#0b72b9';
 const arcOrange = '#ffba60';
+// const fontFamily = ['Roboto', 'Helvetica', 'Arial', 'sans-serif'];
+// const fontFamily1 = ['Orbitron', 'Helvetica', 'Arial', 'sans-serif'];
 
 export default createMuiTheme({
   palette: {
     common: {
-      arcBlue: arcBlue,
-      arcOrange: arcOrange,
+      blue: arcBlue,
+      orange: arcOrange,
     },
     primary: {
       main: arcBlue,
+    },
+    secondary: {
+      main: arcOrange,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: 'Raleway',
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '1rem',
+    },
+    estimate: {
+      fontFamily: 'Pacifico',
+      fontSize: '1rem',
+      textTransform: 'none',
+      color: 'white',
     },
   },
 });
