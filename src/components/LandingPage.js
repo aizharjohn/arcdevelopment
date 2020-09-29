fd0c4e2568805aad590c8e26e5890e141073105f
Aizhar John Quindoza <aizhar.quindoza@gmail.com>
2020-09-29 06:16:40 +0400

    Finished the revolution block. Started information block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles, useTheme } from '@material-ui/styles';
-import { Grid, Button, Typography, useMediaQuery } from '@material-ui/core';
+import {
+  Grid,
+  Button,
+  Typography,
+  useMediaQuery,
+  Card,
+  CardContent,
+} from '@material-ui/core';
 import ButtonArrow from './ui/ButtonArrow';
 
 import animationData from '../animations/landinganimation/data';
 import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
 import websitesIcon from '../assets/websiteIcon.svg';
+import revolutionBackground from '../assets/repeatingBackground.svg';
+import infoBackground from '../assets/infoBackground.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -83,6 +92,36 @@ const useStyles = makeStyles((theme) => ({
       padding: 25,
     },
   },
+  revolutionBackground: {
+    backgroundImage: `url(${revolutionBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%',
+  },
+  revolutionCard: {
+    position: 'absolute',
+    boxShadow: theme.shadows[10],
+    borderRadius: 15,
+    padding: '10em',
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: '8em',
+      paddingBottom: '8em',
+      paddingLeft: 0,
+      paddingRight: 0,
+      borderRadius: 0,
+      width: '100%',
+    },
+  },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%',
+  },
 }));
 
 const LandingPage = () => {
@@ -92,7 +131,7 @@ const LandingPage = () => {
 
   const defaultOptions = {
     loop: true,
-    autoplay: true,
+    autoplay: false,
     animationData,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
@@ -250,6 +289,78 @@ const LandingPage = () => {
               alt="website icon"
             />
           </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*-----The Revolution Block-----*/}
+        <Grid
+          container
+          style={{ height: '100em', marginTop: '12em' }}
+          alignItems="center"
+          justify="center"
+        >
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                style={{ textAlign: 'center' }}
+              >
+                <Grid item>
+                  <Typography variant="h3" gutterBottom>
+                    The Revolution
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">
+                    Visionary insights coupled with cutting-edge technology is a
+                    recipe for revolution.
+                  </Typography>
+                  <Button
+                    variant="outlined"
+                    className={classes.learnButtonHero}
+                  >
+                    <span style={{ marginRight: 10 }}> Learn More</span>
+                    <ButtonArrow
+                      width={15}
+                      height={15}
+                      fill={theme.palette.common.blue}
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBackground} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*-----Information Block-----*/}
+        <Grid
+          container
+          style={{ height: '80em' }}
+          alignItems="center"
+          direction="row"
+        >
+          <Grid item style={{ position: 'absolute', marginLeft: '5em' }}>
+            <Grid container direction="column">
+              <Typography variant="h2" style={{ color: 'white' }}>
+                About Us
+              </Typography>
+              <Typography variant="subtitle2">Let's get personal</Typography>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  className={classes.learnButton}
+                  style={{ color: 'white', borderColor: 'white' }}
+                >
+                  <span style={{ marginRight: 10 }}> Learn More</span>
+                  <ButtonArrow width={10} height={10} fill="white" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <div className={classes.infoBackground} />
         </Grid>
       </Grid>
     </Grid>
